bae512dd17be69047c388c0f4e5e198e7312426d
amirmn8 <amirmn8@gmail.com>
2023-06-10 14:45:47 +0330

    add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,186 @@
+<template >
+    <div>
+        <div class="bg-white dark:bg-black md:bg-slate-100">
+
+            <CommonNavbar :switch-to-persian="switchToPersian" :switch-to-english="switchToEnglish" :state="state" />
+            <div class="dark:bg-black bg-white">
+        
+                <div class="2xl:mx-36 pt-16">
+                    <div class="flex flex-col items-center gap-4">
+                        <img class="lg:w-1/2" src="../assets/images/404-Image.png" alt="">
+                        <p class="font-bold text-2xl">صفحه مورد نظر پیدا نشد (404)</p>
+                        <p>احتمال دارد آدرس صفحه مورد نظر خود را به اشتباه وارد کرده باشید</p>
+                        <a href="/"
+                                    class="hover:bg-ExYellow hover:text-white bg-white text-ExYellow px-12 text-center justify-center border border-solid border-ExYellow dark:text-ExBlack fill-white dark:hover:bg-ExYellow dark:hover:shadow-md dark:hover:shadow-gray-600 flex my-auto items-center rounded-lg h-10">
+
+                                    برو به صفحه اصلی 
+                    </a>
+                    </div>
+
+                </div>
+                <CommonFooter :state="state" />
+            </div>
+
+
+        </div>
+    </div>
+</template>
+<script setup>
+useHead({
+    htmlAttrs: {
+        ref: "darkSwitchRef"
+    },
+    title: 'MoneyEx | Blog',
+})
+useHead({
+    bodyAttrs: {
+        class: "dark:bg-black"
+    }
+})
+onMounted(() => {
+    watchEffect(() => {
+        if (process.client) {
+            if (localStorage.theme) {
+                if (
+                    localStorage.theme === 'dark' ||
+                    (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+                ) {
+                    useHead({
+                        htmlAttrs: {
+                            class: 'dark'
+                        },
+                    })
+                } else {
+                    useHead({
+
+                        htmlAttrs: {
+                            class: ''
+                        },
+                    })
+                }
+            }
+        }
+    });
+});
+
+
+const state = reactive({
+    lang: false,
+    markets: "بازارها",
+    trade: 'تبدیل ارز',
+    userManual: 'راهنمای استفاده',
+    easyShopping: 'خرید آسان',
+    blog: 'بلاگ',
+    main: 'صفحه اصلی',
+    about: 'درباره ی ما',
+    contact: 'تماس با ما',
+    rules: 'قوانین و مقررات',
+    back: 'بازگشت',
+    userPanel: 'حساب کاربری',
+    footerText1: 'درباره ی مانی اکس',
+    footerText2: 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده',
+    footerText3: "دسترسی های سریع",
+    footerText4: "درباره ما",
+    footerText5: "تماس با ما",
+    footerText6: "حساب کاربری",
+    footerText7: "قوانین و مقررات",
+    footerText8: "خدمات ما",
+    footerText9: "تبدیل ارز",
+    footerText10: "بلاگ",
+    footerText11: "ارتباط با ما",
+    footerText12: "استان تهران، شهر تهران، خیابان مرکزی ساختمان مرکزی، پلاک 7",
+    footerText13: "کلیه حقوق این سایت محفوظ و متعلق به مانی اکس می باشد",
+    hash: '#fa',
+
+
+})
+
+const switchToPersian = () => {
+    window.location.hash = "#fa";
+    state.lang = true;
+    state.markets = "بازار ها";
+    state.trade = "تبدیل ارز";
+    state.userManual = "راهنمای استفاده";
+    state.easyShopping = "خریدآسان";
+    state.blog = "بلاگ";
+    state.main = "صفحه ی اصلی";
+    state.about = "درباره ما";
+    state.contact = "تماس با ما";
+    state.rules = "قوانین و مقررات";
+    state.back = "بازگشت";
+    state.userPanel = "حساب کاربری";
+    state.footerText1 = 'درباره ی مانی اکس';
+    state.footerText2 = 'لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد کتابهای زیادی در شصت و سه درصد گذشته حال و آینده';
+    state.footerText3 = "دسترسی های سریع";
+    state.footerText4 = "درباره ما";
+    state.footerText5 = "تماس با ما";
+    state.footerText6 = "حساب کاربری";
+    state.footerText7 = "قوانین و مقررات";
+    state.footerText8 = "خدمات ما";
+    state.footerText9 = "تبدیل ارز";
+    state.footerText10 = "بلاگ";
+    state.footerText11 = "ارتباط با ما";
+    state.footerText12 = "استان تهران، شهر تهران، خیابان مرکزی ساختمان مرکزی، پلاک 7";
+    state.footerText13 = "کلیه حقوق این سایت محفوظ و متعلق به مانی اکس می باشد";
+    state.hash = '#fa',
+
+        useHead({
+            bodyAttrs: {
+                dir: 'rtl'
+            },
+        })
+}
+const switchToEnglish = () => {
+    window.location.hash = "#en";
+    state.lang = false;
+    state.markets = "Markets";
+    state.trade = "Trade";
+    state.userManual = "User Manual";
+    state.easyShopping = "Easy Buy";
+    state.blog = "Blog";
+    state.main = "Home";
+    state.about = "About Us";
+    state.contact = "Contact Us";
+    state.rules = "Terms and Conditions";
+    state.back = "Back";
+    state.userPanel = "User Panel";
+    state.footerText1 = "About MoneyEx";
+    state.footerText2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque In egestas erat imperdiet sed euismod nisi porta lorem mollis Morbi tristique senectus et netus Mattis pellentesque id nibh tortor id aliquet lectus proin";
+    state.footerText3 = "Quick Accesses";
+    state.footerText4 = "AboUT Us";
+    state.footerText5 = "Contact Us";
+    state.footerText6 = "User Panel";
+    state.footerText7 = "Terms and Conditions";
+    state.footerText8 = "Our Services";
+    state.footerText9 = "Exchange";
+    state.footerText10 = "Blog";
+    state.footerText11 = "Contact Us";
+    state.footerText12 = "Tehran province, Tehran city, st Central building Central, no.7";
+    state.footerText13 = "All rights of this site are reserved and belong to MoneyEx";
+    state.hash = '#en'
+
+
+    useHead({
+        bodyAttrs: {
+            dir: 'ltr'
+        }
+
+    })
+}
+onMounted(() => {
+    var hash = window.location.hash;
+    if (hash) {
+        if (hash === "#en") {
+            switchToEnglish();
+        }
+        else {
+            switchToPersian();
+        }
+    }
+    else {
+        switchToPersian();
+    }
+});
+
+        // پایان بخش تغییر زبان
+</script>
